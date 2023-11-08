f3b6ed1f288fe73083fdc5dad126b64f7443e6e9
zhamdi <zied@weally.org>
2023-11-08 12:18:10 +0100

    Types are now declared in separate indexing files called types.ts

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -2,6 +2,6 @@ export { AlreadySubscribedError } from "./AlreadySubscribedError";
 export { DuplicateMatchesError } from "./DuplicateMatchesError";
 export { EntityNotFoundError } from "./EntityNotFoundError";
 export { InvalidOrderError } from "./InvalidOrderError";
-export { InvalidPaymentError } from "./InvalidPaymentError";
+export { InvalidPaymentError, PaymentErrorCode, PaymentErrorDetails } from "./InvalidPaymentError";
 export { PaymentError } from "./PaymentError";
 export { SystemError } from "./SystemError";
