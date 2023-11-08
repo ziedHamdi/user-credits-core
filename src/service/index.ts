f9501bb361a9aeca6cb120de685bd82217659947
zhamdi <zied@weally.org>
2023-11-08 10:41:08 +0100

    Added BaseService.ts and PaymentService.ts without the mechanisms to load an IPaymentClient implementation and without jest tests

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,2 +1,4 @@
 export type { IPaymentClient } from "./IPaymentClient";
 export type { IService } from "./IService";
+export {BaseService} from "./BaseService"
+export {PaymentService} from "./PaymentService"
