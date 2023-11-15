58e5a867079dd8b9c21fc13063c9c4d467ea3b39
zhamdi <zied@weally.org>
2023-11-15 09:44:35 +0100

    publishing version

--- src/service/PaymentService.ts
+++ src/service/PaymentService.ts
@@ -26,17 +26,19 @@ export class PaymentService<K extends IMinimalId> extends BaseService<K> {
   }
 
   async createOrder(
-      offerId: K,
-      userId: K,
-      quantity?: number, // Optional quantity parameter
-      currency: string = this.defaultCurrency,
+    offerId: K,
+    userId: K,
+    quantity?: number, // Optional quantity parameter
+    currency: string = this.defaultCurrency,
   ): Promise<IOrder<K>> {
     const order = await super.createOrder(offerId, userId, quantity, currency);
     const paymentIntent = await this.paymentClient.createPaymentIntent(order);
-    if(!paymentIntent)
-      throw new InvalidPaymentError("Failed to create payment intent", {orderId:order._id})
+    if (!paymentIntent)
+      throw new InvalidPaymentError("Failed to create payment intent", {
+        orderId: order._id,
+      });
 
-    return paymentIntent
+    return paymentIntent;
   }
 
   async afterExecute(order: IOrder<K>): Promise<IUserCredits<K>> {
